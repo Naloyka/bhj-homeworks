97c8408cc354dc55986c899ec653b4fd8c104af4
Naloyka <elena.naloeva@yandex.ru>
2022-05-06 17:54:55 +0300

    Задача выпадающее меню

---
+++ element-search/menu/task.js
@@ -0,0 +1,12 @@
+const link = document.getElementsByClassName("menu__link")
+
+for (let i = 0; i < link.length; i++) {
+
+    link[i].onclick = () => {
+        let menuSub = link[i].closest(".menu__item").querySelector('.menu_sub')
+        if (menuSub !== null) {
+            menuSub.classList.toggle("menu_active")
+            return false
+        }
+    }
+}

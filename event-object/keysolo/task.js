37ad31f900682227cb07b7b8bba1998ecc5f75f6
Naloyka <elena.naloeva@yandex.ru>
2022-05-07 15:58:16 +0300

    задача Key solo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,6 +17,25 @@ class Game {
   }
 
   registerEvents() {
+
+    document.addEventListener('keyup', (event) => {
+      let text = this.currentSymbol.textContent
+      let key = event.key
+
+    if( text == key) {
+      this.success();
+    } else {
+      this.fail()
+    }
+      
+    })
+      
+      
+
+
+
+
+
     /*
       TODO:
       Написать обработчик события, который откликается
@@ -56,18 +75,18 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -77,7 +96,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;

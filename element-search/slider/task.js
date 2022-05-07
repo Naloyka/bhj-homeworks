39e6e49a2580d266b8ae43bb522df72a021fe1a4
Naloyka <elena.naloeva@yandex.ru>
2022-05-07 14:01:34 +0300

    Задача slider

---
+++ element-search/slider/task.js
@@ -0,0 +1,39 @@
+const prev = document.querySelector(".slider__arrow_prev")
+const next = document.querySelector(".slider__arrow_next")
+
+const slider = Array.from(document.getElementsByClassName("slider__item"))
+
+next.onclick = () => {
+    for (let i = 0; i < slider.length; i++) {
+
+        if (slider[i].className.includes("slider__item_active")) {
+            slider[i].classList.remove("slider__item_active")
+
+            if (i === (slider.length - 1)) {
+                i = 0
+                slider[i].classList.add("slider__item_active")
+            } else {
+                i++
+                slider[i].classList.add("slider__item_active")
+            }
+        }
+    }
+}
+
+prev.onclick = () => {
+    for (let i = slider.length - 1; i >= 0; i--) {
+
+        if (slider[i].className.includes("slider__item_active")) {
+            slider[i].classList.remove("slider__item_active")
+
+            if (i === 0) {
+                i = slider.length - 1;
+                slider[i].classList.add("slider__item_active")
+            } else {
+                i--
+                slider[i].classList.add("slider__item_active")
+            }
+        }
+    }
+}
+

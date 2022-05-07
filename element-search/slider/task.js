05f25de1d7dce7702bdd4910e37c18e71c42e352
Naloyka <elena.naloeva@yandex.ru>
2022-05-08 00:50:28 +0300

    исправлен слайдер

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,8 +2,30 @@ const prev = document.querySelector(".slider__arrow_prev")
 const next = document.querySelector(".slider__arrow_next")
 
 const slider = Array.from(document.getElementsByClassName("slider__item"))
+let idxActive;
 
 next.onclick = () => {
+    idxActive = slider.findIndex(item => item.className.includes("slider__item_active"))
+    slider[idxActive].classList.remove("slider__item_active")
+    idxActive++
+    if (idxActive === slider.length - 1) {
+        idxActive = 0
+    }
+    slider[idxActive].classList.add("slider__item_active")
+}
+
+prev.onclick = () => {
+    idxActive = slider.findIndex(item => item.className.includes("slider__item_active"))
+    slider[idxActive].classList.remove("slider__item_active")
+    idxActive--
+    if (idxActive === -1) {
+        idxActive = slider.length - 1
+    }
+    slider[idxActive].classList.add("slider__item_active")
+}
+
+
+/*next.onclick = () => {
     for (let i = 0; i < slider.length; i++) {
 
         if (slider[i].className.includes("slider__item_active")) {
@@ -35,5 +57,5 @@ prev.onclick = () => {
             }
         }
     }
-}
+}*/
 

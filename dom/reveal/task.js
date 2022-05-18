8d17f557b523adcde25238450aa0350781bebca3
Naloyka <elena.naloeva@yandex.ru>
2022-05-18 19:19:44 +0300

    показать элеметы при scroll

---
+++ dom/reveal/task.js
@@ -0,0 +1,20 @@
+const reveal = Array.from(document.querySelectorAll(".reveal"))
+
+const visible = () => {
+    reveal.forEach(item => {
+        const { top, bottom } = item.getBoundingClientRect()
+        if (top < 0) {
+            item.classList.add("reveal_active")
+        } else if (bottom < window.innerHeight) {
+            item.classList.add("reveal_active")
+        } else {
+            item.classList.remove("reveal_active")
+        }
+    })
+}
+
+window.addEventListener('scroll', visible)
+
+
+
+
